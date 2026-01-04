02baa2e3667dd622ba23ba9c8c6b020a645f41a1
Tomleb987 <142760139+Tomleb987@users.noreply.github.com>
2026-01-04 08:56:04 -1000

    Update browser.ts

--- src/lib/supabase/browser.ts
+++ src/lib/supabase/browser.ts
@@ -1,5 +1,7 @@
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
 
 export function supabaseBrowser() {
-  return createPagesBrowserClient();
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  return createBrowserClient(url, anonKey);
 }
